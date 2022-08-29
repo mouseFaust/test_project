fcaf94f9ac42eff85c5c9ca307cf14e924a14eec
Danilova Irina <Faust1814@yandex.ru>
2022-08-29 20:33:01 +0400

    fix task2

--- task2/main.py
+++ task2/main.py
@@ -1,15 +1,19 @@
-try:
-    print("Введите массив значений: ")
-    array = [int(float(arr)) for arr in input().split()]
-    central_value = int(sum(array) / len(array))
-    counter = 0
-    for arr in array:
-        while arr != central_value:
-            if arr < central_value:
-                arr += 1
-            else:
-                arr -= 1
-            counter += 1
-    print(f"{counter}")
-except:
-    print("Введены неверные данные")
+
+print("Введите путь до файла: ")
+way = input()
+with open(f"{way}", "r") as file:
+    try:
+        array1 = [arr for arr in file.readlines()]
+        array = [int(float(arr)) for arr in array1 if arr != "\n"]
+        central_value = int(sum(array) / len(array))
+        counter = 0
+        for arr in array:
+            while arr != central_value:
+                if arr < central_value:
+                    arr += 1
+                else:
+                    arr -= 1
+                counter += 1
+        print(f"{counter}")
+    except:
+        print("Введены неверные данные")
